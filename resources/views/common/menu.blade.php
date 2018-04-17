f440bc2a4b8f1e6d50efaa5620493ff661f440e9
Rinjax <steve@encara.co.uk>
2018-04-17 21:19:11 +0100

    first

---
+++ resources/views/common/menu.blade.php
@@ -0,0 +1,13 @@
+<nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+        <div class="pull-left navbar-text"><strong>South Downs Hang Gliding</strong></div>
+        <div class="pull-right">
+            <ul class="nav navbar-nav navbar-link">
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Courses</a></li>
+                <li><a href="#">Holidays</a></li>
+                <li><a href="#">Gallery</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,43 @@
+@extends('frame')
+
+@section('body')
+
+    @include('common.menu')
+    <div class="jumbotron banner" style="background-image: url('/img/flypast.jpg'); min-height: 50rem">
+
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-md-9">
+                <h2>Why Hang Gliders?</h2>
+                <p>
+                    Hang Gliders really fly ... right up to the clouds in thermals and away on flights of several hours.
+                </p>
+                <p>
+                    The graceful flight of a hang glider with the aerodynamic pilot position really is flying like the birds.
+                    Simply leaning the way you want to go the weight-shift control gives a wonderful natural feeling as
+                    you swoop and glide and is totally unique to hang gliding.
+                </p>
+                <p>
+                    Hang Gliders have a fantastic glide performance and an impressive speed range. High performance hang
+                    gliders can reach 80mph!
+                </p>
+                <p>
+                    Unlike paragliders they won't collapse in turbulent air but maintain shape by their internal airframe.
+                    The twist and sweepback of the wing provides a natural stability allowing hang gliders to endure the
+                    roughest of flight conditions!
+                </p>
+            </div>
+            <div class="col-xs-12 col-md-3 hidden-xs">
+                <h2>Contact Us</h2>
+                <p>South Downs Hang Gliding</p>
+                <p>Gibraltar Farm, Firle</p>
+                <p>East Sussex</p>
+                <p>BN8 6NB</p>
+                <p>Tel. 07890 362648</p>
+                <p><a href="mailto:info@southdownshanggliding.co.uk">info@southdownshanggliding.co.uk</a></p>
+            </div>
+        </div>
+    </div>
+
+@endsection
